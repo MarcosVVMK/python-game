f5187c3880414a9437201830c06d3af6c8494aba
Marcos <marcos.matos@academico.ifg.edu.br>
2021-09-10 10:07:06 -0300

    improve forca game

--- jogos.py
+++ jogos.py
@@ -1,17 +1,21 @@
 import forca
-import advinhacao
+import adivinhacao
 
-print("*********************************")
-print("*** Escolha o jogo que deseja ***")
-print("*********************************")
+def escolhe_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
 
-print("(1) Forca (2) Advinhação")
+    print("(1) Forca (2) Adivinhação")
 
-jogo = int(input("Qual Jogo?"))
+    jogo = int(input("Qual jogo? "))
 
-if(jogo == 1):
-    print("Jogando Forca")
-    forca.jogar()
-elif(jogo == 2):
-    print("Jogando Advinhação")
-    advinhacao.jogar()
+    if(jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
+
+if(__name__ == "__main__"):
+    escolhe_jogo()
