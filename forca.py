6f0142315da3cce50d85675f8ae19c52e5dd64a4
Marcos <marcos.matos@academico.ifg.edu.br>
2021-11-08 20:24:50 -0300

    update secret word

--- forca.py
+++ forca.py
@@ -3,25 +3,33 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "maça".upper()
+    letras_acertadas = []
+    
+    for letra in palavra_secreta:
+        letras_acertadas.append("_")
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
-
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+        enforcou = erros == 6   
+        acertou = '_' not in letras_acertadas 
         print(letras_acertadas)
 
     print("Fim do jogo")
